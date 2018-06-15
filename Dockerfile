a7fd686497bd76ca52408ed7338d338a1f13921f
Jie Su <jiesu12@gmail.com>
2018-06-15 12:07:50 -0500

    simplify - remove ENTRYPOINT script, move those commands to Dockerfile, no need to generate sshd keys.

--- Dockerfile
+++ Dockerfile
@@ -4,13 +4,21 @@ RUN apt-get update \
 && echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections \
 && apt-get -y install openssh-server gitolite3
 
+# sshd won't start without this folder
+RUN mkdir /var/run/sshd
+# Disable unwanted authentications
+RUN sed -i -E -e 's/^#?(\w+Authentication)\s.*/\1 no/' -e 's/^(PubkeyAuthentication) no/\1 yes/' /etc/ssh/sshd_config
+# Disable sftp subsystem
+RUN sed -i -E 's/^Subsystem\ssftp\s/#&/' /etc/ssh/sshd_config
+
+# Setup git user
 RUN useradd -ms /bin/bash git
+RUN chown -R git:git ~git
 
+# Setup gitolite
 COPY gitolite-admin /tmp/gitolite-admin
-
-COPY start.sh /
-ENTRYPOINT ["/start.sh"]
+RUN su - git -c "gitolite setup -pk \"/tmp/gitolite-admin/keydir/jie.pub\""
 
 EXPOSE 22
 
-CMD ["sshd"]
+CMD ["/usr/sbin/sshd", "-D"]
--- start.sh
+++ start.sh
@@ -1,31 +1,4 @@
 #!/bin/sh
 
-echo 'start ENTRYPOINT'
 
-set -- /usr/sbin/sshd -D
-
-# unlike most of the google search results, keys are supposed to be under this folder
-mkdir -p /etc/ssh/keys
-# Setup SSH HostKeys
-for algorithm in rsa dsa ecdsa ed25519
-do
-  keyfile=/etc/ssh/keys/ssh_host_${algorithm}_key
-  [ -f $keyfile ] || ssh-keygen -q -N '' -f $keyfile -t $algorithm
-  grep -q "HostKey $keyfile" /etc/ssh/sshd_config || echo "HostKey $keyfile" >> /etc/ssh/sshd_config
-done
-chmod -R go-r /etc/ssh/keys
-# Disable unwanted authentications
-sed -i -E -e 's/^#?(\w+Authentication)\s.*/\1 no/' -e 's/^(PubkeyAuthentication) no/\1 yes/' /etc/ssh/sshd_config
-# Disable sftp subsystem
-sed -i -E 's/^Subsystem\ssftp\s/#&/' /etc/ssh/sshd_config
-
-chown -R git:git ~git
-# run gitolite setup as git user
-su - git -c "gitolite setup -pk \"/tmp/gitolite-admin/keydir/jie.pub\""
-
-# sshd won't start without this
-mkdir -p /var/run/sshd
-
-echo 'end ENTRYPOINT'
-
-exec "$@"
+/usr/sbin/sshd -D
